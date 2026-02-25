c7c8bcbc8c45eb250e5d11f318e99f7e56d95752
Marvin <marvinbernard2018@gmail.com>
2026-02-25 14:57:44 +0300

    feat: add UserViewSet with login endpoint

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions 
+from rest_framework.response import Response 
+from rest_framework.decorators import action 
+from django.contrib.auth import authenticate 
+from rest_framework.authtoken.models import Token 
+from .models import User 
+from .serializers import UserSerializer
 # Create your views here.
